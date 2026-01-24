7564cdebf74fd1b0184d1e3ac47814f84f43e43c
AVIS10060 <avisharmaid@gmail.com>
2026-01-24 11:28:59 +0530

    FindMaximum

---
+++ practise.js/FindMaximum.js
@@ -0,0 +1,20 @@
+
+
+const FindMaximum = (maxi) =>{
+
+   
+
+    let maximum = (Math.max(...maxi))
+
+    return maximum
+    
+}
+
+
+
+
+
+let arr = [-1,-4,-5,-2,-6,-9]
+console.log(FindMaximum(arr))
+
+
