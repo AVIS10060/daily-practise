90ee4ed2a8b0d972e0ef61f04259c3115af3b8d2
AVIS10060 <avisharmaid@gmail.com>
2026-02-18 13:38:51 +0530

    Password Validation

---
+++ practise.js/PasswordValidation.js
@@ -0,0 +1,30 @@
+const PV = (str) =>{
+
+    let hasLowerCase = false 
+    let hasUpperCase = false 
+    let hasNumber = false
+
+    for(let char of str ){
+        if(char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90){
+            hasUpperCase = true 
+        }
+        else if(char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122){
+            hasLowerCase = true 
+        }
+        else if(!isNaN(Number(char))){
+            hasNumber = true  
+        }
+
+    }
+    if(!hasLowerCase || !hasUpperCase || !hasNumber || str.length < 8){
+        return false 
+    }
+
+    return true 
+
+
+
+}
+
+console.log(PV('abcdefghknkR1'))
+console.log(PV('avc'))
